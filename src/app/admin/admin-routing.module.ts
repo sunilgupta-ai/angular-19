99fe55bcce1dfaa549779d634637c1fd7ced0ca9
sunilgupta-ai <sunilgupta.libraries@gmail.com>
2025-04-10 18:19:28 +0530

    login,registration and dashboard page

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   { path:'',component:AdminComponent},
   { path:'login',component:LoginComponent},
+  { path:'register',component:RegisterComponent},
+  { path:'dashboard',component:DashboardComponent},
 ];
 
 @NgModule({
